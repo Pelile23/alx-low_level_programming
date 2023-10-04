6197c322c054edc8d6a6ba3004e068466563c01c
Pelile23 <pelile.chintimbwe@gmail.com>
2023-10-04 10:10:07 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,11 +16,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *p;
 
-	p = malloc(sizof(char) * size);
-	if (size == 0 || p = NULL)
+	p = malloc(sizeof(char) * size);
+	if (size == 0 || p == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
 		p[i] = c;
-	return (p)
+	return (p);
 }
