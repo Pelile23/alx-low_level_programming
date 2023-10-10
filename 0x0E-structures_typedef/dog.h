0715acc970d4ecb423c30eeca535d94ded2d0cd6
Pelile23 <pelile.chintimbwe@gmail.com>
2023-10-10 07:30:14 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - new type
+ *
+ * @name: name to be given
+ * @age: student age
+ * @owner: owner
+ *
+ */
+
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
 
 #endif
